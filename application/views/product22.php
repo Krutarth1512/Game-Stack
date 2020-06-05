df3a8bb221d2b25b5b1eecb3f5c0423090924317
Abhishek Kathuria <30931060+bit22bit@users.noreply.github.com>
2020-06-05 19:43:15 -0400

    footer attaching

--- application/views/product22.php
+++ application/views/product22.php
@@ -80,35 +80,4 @@
 
         </div>
 
-        <footer class="footer">
-
-            <div class="footer-inner">
-
-                <p>&copy; Copyright
-                    <a href="#"> About</a> &#124;
-                    <a href="contactUs.html"> Contact Us</a> &#124;
-                    <a href="#"> Support </a> &#124;
-                    <a href="#">Terms of Use</a> &#124;
-                    <a href="#">Privacy Policy</a>
-                </p>
-
-                <p>
-                    <h4>Get To Know Us :</h4>
-                </p>
-
-                <p>
-                    <a href=""> <img src="icons/social/twitter.png" width="3%" title="Twitter-GameShop">
-                    <a href=""> <img src="icons/social/youtube.png" width="3%" title="Youtube-GameShop">
-                    <a href=""> <img src="icons/social/instagram.png" width="3%" title="Instagram-GameShop">
-                </p>
-
-                <div class="clr"></div>
-
-            </div>
-
-        </footer>
-
-    </div>
-</body>
-
-</html>
+<?php $this->load->view('inc/footer.php'); ?>
