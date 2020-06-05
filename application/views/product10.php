7e72eba55e1810aa10d3df7c098d577aaf64c058
Abhishek Kathuria <30931060+bit22bit@users.noreply.github.com>
2020-06-05 19:39:09 -0400

    front changes

---
+++ application/views/product10.php
@@ -0,0 +1,79 @@
+<?php $this->load->view('inc/header.php'); ?>
+
+        <div class="feature"></div>
+
+        <div class="container">
+            <img src="<?php echo base_url();?>assets/images/unocover.jpg" alt="UNO" style="width:100%;">
+        </div>
+
+        <div class="row">
+
+            <div class="columnProduct">
+
+                <img src="<?php echo base_url();?>assets/images/unoHome.jpg" width="70%" class="center">
+                <h1 class="center">UNO</h1>
+                <h3 class="center">Release Date : <font style="color: teal;"> May 9, 2006 </font></h3>
+                <h3 class="center">Type : <font style="color: teal;"> Cards </font></h3>
+                <h2 class="center" style="color: orange; font-size: 3vw;"> C$ 9.99 </h2>
+
+                <div class="center">
+                    <button class="edition-button" style="font-size: 15px;">ADD TO CART</button>
+                </div>
+
+            </div>
+
+            <div class="columnProduct">
+
+                <h2>Description</h2>
+                <p style="color: black; font-size: 1vw;">
+					In UNO, Match cards either by matching color or value and play action cards to change things up. Race against others to empty your hand before everyone else in either Classic Play or customize your experience with a variety of House Rules and match settings to ensure you and your friends never play the ame game twice.
+				</p>
+
+                <h2 style="padding-top: 20px;">Edition</h2>
+                <button class="edition-button" style="font-size: 15px;">Standard Edition</button>
+                <button class="edition-button-simple" style="font-size: 15px;">Digital Deluxe Edition</button>
+                <button class="edition-button-simple" style="font-size: 15px;">Gold Edition</button>
+
+                <h2 style="padding-top: 30px;">Platform</h2>
+                <button class="edition-button" style="font-size: 15px;">PC (Download)</button>
+                <button class="edition-button-simple" style="font-size: 15px;">PS4</button>
+                <button class="edition-button-simple" style="font-size: 15px;">Xbox One</button>
+            </div>
+
+            <div class="columnProduct">
+                <h2>System Requirements : </h2>
+
+                <div class="boxed">
+
+                    <h2>MINIMUM:</h2>
+
+                    <ul>
+                        <li>OS: <font style="color: black;"> Windows 7, Windows 8.1 or Windows 10 (64bit versions only) </font></li>
+                        <li>Processor: <font style="color: black;"> Intel Pentium G860, AMD FX-4150 or equivalent </font></li>
+                        <li>Graphics: <font style="color: black;"> NVIDIA GeForce GTX 550 ti, AMD Radeon HD 6670 or better  </font></li>
+                        <li>System RAM: <font style="color: black;"> 4 GB </font></li>
+                    </ul>
+
+                    <h2>RECOMMENDED:</h2>
+
+                    <ul>
+                        <li>OS: <font style="color: black;"> ​Windows 7, Windows 8.1, Windows 10 (64bit version only) </font></li>
+                        <li>Processor: <font style="color: black;"> Intel Core i3-2100, AMD FX-6300 or equivalent </font></li>
+                        <li>Graphics: <font style="color: black;"> NVIDIA GeForce GTX 950, AMD Radeon R7 270 or better </font></li>
+                        <li>System RAM: <font style="color: black;"> 6 GB </font></li>
+                    </ul>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="footerblurb">
+
+            <div class="footerblurb-inner">
+                <div class="clr"></div>
+            </div>
+
+        </div>
+
+        <?php $this->load->view('inc/footer.php'); ?>
